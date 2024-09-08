89b7df4b0901e57bb51443eea9c57894f994e8d4
Evan Chen <bugena123@gmail.com>
2024-09-08 00:11:28 -0700

    commit

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+
+
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Hide all slides
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 } // Reset to first slide if at the end
+    slides[slideIndex - 1].style.display = "block"; // Show current slide
+    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+showSlides(); // Initial call to start the slideshow
+
+
+// animate intro
+document.addEventListener('DOMContentLoaded', () => {
+    const element = document.getElementById('intro');
+    const text = element.textContent;
+    const letters = text.split('');
+    
+    element.innerHTML = letters.map(letter => `<span>${letter}</span>`).join('');
+});
