9801eb47d0a120e210c749d36e9932c76b05fc21
Evan Chen <bugena123@gmail.com>
2024-09-11 16:41:11 -0700

    fixes fetch link again

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ async function checkSectionExists(date) {
     const sectionId = date; // Assumes section ID matches the date format
     console.log(sectionId)
     try {
-        const response = await fetch('/thoughts/thoughts.html');
+        const response = await fetch('https://evanchen11.github.io/web/thoughts/thoughts.html');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
